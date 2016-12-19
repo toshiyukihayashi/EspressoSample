e90739718f4bf60a238d7d02db89944c1f7ca587
hayashi <xhayashi.toshiyuki3@contract.isid.co.jp>
2016-12-19 19:06:00 +0900

    no message

--- app/src/main/java/circleci/espressosample/MainActivity.java
+++ app/src/main/java/circleci/espressosample/MainActivity.java
@@ -1,10 +1,16 @@
 package circleci.espressosample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.goodpatch.feedbacktool.sdk.Balto;
+import com.testfairy.TestFairy;
+
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +19,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button changeTextButton = (Button)findViewById(R.id.changeTextButton);
-        final TextView textView = (TextView)findViewById(R.id.helloWorldTextView);
+        TestFairy.begin(this, "882114e169dd5278ba361f991e58cb89fa133a9b");
+        TestFairy.sendUserFeedback("test add user feedback");
+        TestFairy.addCheckpoint("test add checkpoint");
+        TestFairy.addEvent("test add event");
 
-        changeTextButton.setOnClickListener(new Button.OnClickListener() {
+        CrashManager.register(this);
+        UpdateManager.register(this);
+
+//        DeployGate.install(this.getApplication());
+//
+//        ((TextView)findViewById(R.id.textAuthorName)).setText(DeployGate.getAuthorUsername());
+        ((TextView)findViewById(R.id.textLoginuserName)).setText("");
+//
+        ((Button)findViewById(R.id.LoginfoButton)).setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                textView.setText("Text changed!");
+                ((TextView)findViewById(R.id.textAuthorName)).setText("Log Info button CLICK!");
             }
         });
+        ((Button)findViewById(R.id.LogdebugButton)).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.textAuthorName)).setText("Log Debug button CLICK!");
+            }
+        });
+//        ((Button)findViewById(R.id.LogVerboseButton)).setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                DeployGate.logVerbose("DeployGate EspressoSample [ Log Verbose ]");
+//            }
+//        });
+//        ((Button)findViewById(R.id.LogwarnButton)).setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                DeployGate.logWarn("DeployGate EspressoSample [ Log Warn ]");
+//            }
+//        });
+//        ((Button)findViewById(R.id.LogerrorButton)).setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View v) {
+//                DeployGate.logWarn("DeployGate EspressoSample [ Log Error ]");
+//            }
+//        });
+
+        Balto.init(this.getApplication());
+//        BaltoApi.getInstance().postFeedback(String apiToken, String gitHubToken, String buildId, BaltoPostFeedbackRequest baltoPostFeedbackRequest)
     }
 }
